197fa93771917d3924a843a455365c7973a92a3d
Alizander <92832144+Alizander@users.noreply.github.com>
2021-10-19 20:26:43 -0400

    Ejercicio práctico C# - Primer Corte

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Primer_Proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 6;
+            byte b = 9;
+            byte c = 4;
+
+            //aacb
+            Console.WriteLine($"Primera Combinación: {a}{a}{c}{b}");
+            Console.WriteLine("----");
+
+            //bca
+            Console.WriteLine($"Segunda Combinación: {b}{c}{a}");
+            Console.WriteLine("----");
+
+            //bcbc
+            Console.WriteLine($"Tercera Combinación: {b}{c}{b}{c}");
+
+            /*Irvin Sánchez
+            C.I.: 28232012
+            Carrera: Ing. en Información
+            */
+        }
+    }
+}
